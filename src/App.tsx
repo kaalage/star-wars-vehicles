3f8a1abfe7fdbd3be4baa46bb75bf642ede7f78a
Karina Lage <karinar_lage@live.com>
2023-07-02 17:22:07 -0500

    Created vehicles list with data from SWAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { FormControl, InputLabel, MenuItem, Select, Button, Box } from '@mui/material';
+import { SelectChangeEvent } from '@mui/material/Select';
 import axios from 'axios';
 import './App.css';
+import logo from './images/logo.png';
 
 interface Item {
   id: number;
@@ -9,6 +12,7 @@ interface Item {
 
 const App: React.FC = () => {
   const [data, setData] = useState<Item[]>([]);
+  const [selectedItem, setSelectedItem] = useState<string>('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,12 +36,50 @@ const App: React.FC = () => {
     fetchData().catch(err => console.log(err));
   }, []);
 
+  const handleSelectChange = (event: SelectChangeEvent<string>) => {
+    setSelectedItem(event.target.value as string);
+  };
+  
+
   return (
     <div className="background-image">
-      {/* Render the data */}
-      {data.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      
+      <div className="center-container">
+    {/* Render the select */}
+    <FormControl variant="outlined">
+      <InputLabel id="select-label">Select a vehicle</InputLabel>
+      <Select
+        labelId="select-label"
+        id="select-vehicle"
+        value={selectedItem}
+        onChange={handleSelectChange}
+        label="Select a vehicle"
+        sx={{ minWidth: '400px', marginBottom: '15px' }}
+      >
+        {data.map((item) => (
+          <MenuItem key={item.id} value={item.name}>
+            {item.name}
+          </MenuItem>
+        ))}
+      </Select>
+      <Box textAlign='center' sx={{ '& button': { m: 1 } }}>
+      <div>
+        <Button 
+        variant="contained" 
+        size="small" 
+        color="primary"
+        sx={{
+        backgroundColor: '#000000', '&:hover': {backgroundColor: '#666'},
+      }}>
+          Checkout
+        </Button>
+      </div>
+    </Box>
+    </FormControl>
+    </div>
+    </header>
     </div>
   );
 };
