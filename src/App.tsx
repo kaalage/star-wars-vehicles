8030318be41a12b8d12afd3524b62ac1db37ddee
Karina Lage <karinar_lage@live.com>
2023-09-25 10:11:53 -0300

    Updated design of home page, changing button and adding a subtitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { FormControl, InputLabel, MenuItem, Select, Button, Box } from '@mui/material';
-import { SelectChangeEvent } from '@mui/material/Select';
-import axios from 'axios';
 import './App.css';
-import logo from './images/logo.png';
-
-interface Item {
-  id: number;
-  name: string;
-}
+import Home from './Home';
+// import VehiclesList from './VehiclesList';
+import CheckoutForm from './CheckoutForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
-  const [data, setData] = useState<Item[]>([]);
-  const [selectedItem, setSelectedItem] = useState<string>('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      let allData: Item[] = [];
-
-      // Initial request
-      let response = await axios.get('https://swapi.dev/api/vehicles/');
-      let apiData: { results: Item[]; next: string } = response.data;
-      allData = allData.concat(apiData.results);
-
-      // Loop through additional pages
-      while (apiData.next) {
-        response = await axios.get(apiData.next);
-        apiData = response.data;
-        allData = allData.concat(apiData.results);
-      }
-
-      setData(allData);
-    };
-
-    fetchData().catch(err => console.log(err));
-  }, []);
-
-  const handleSelectChange = (event: SelectChangeEvent<string>) => {
-    setSelectedItem(event.target.value as string);
-  };
-  
-
   return (
-    <div className="background-image">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      
-      <div className="center-container">
-    {/* Render the select */}
-    <FormControl variant="outlined">
-      <InputLabel id="select-label">Select a vehicle</InputLabel>
-      <Select
-        labelId="select-label"
-        id="select-vehicle"
-        value={selectedItem}
-        onChange={handleSelectChange}
-        label="Select a vehicle"
-        sx={{ minWidth: '400px', marginBottom: '15px' }}
-      >
-        {data.map((item) => (
-          <MenuItem key={item.id} value={item.name}>
-            {item.name}
-          </MenuItem>
-        ))}
-      </Select>
-      <Box textAlign='center' sx={{ '& button': { m: 1 } }}>
-      <div>
-        <Button 
-        variant="contained" 
-        size="small" 
-        color="primary"
-        sx={{
-        backgroundColor: '#000000', '&:hover': {backgroundColor: '#666'},
-      }}>
-          Checkout
-        </Button>
-      </div>
-    </Box>
-    </FormControl>
-    </div>
-    </header>
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/" element={<VehiclesList />} /> */}
+      <Route path="/checkout" element={<CheckoutForm />} />
+    </Routes>
+  </Router>
   );
-};
+}
 
 export default App;
---
+++ src/VehiclesList.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import { FormControl, InputLabel, MenuItem, Select, Button, Box } from '@mui/material';
+import { SelectChangeEvent } from '@mui/material/Select';
+import axios from 'axios';
+import './App.css';
+import logo from './images/logo.png';
+import { Link } from 'react-router-dom';
+
+interface Item {
+  id: number;
+  name: string;
+}
+
+const VehiclesList: React.FC = () => {
+  const [data, setData] = useState<Item[]>([]);
+  const [selectedItem, setSelectedItem] = useState<string>('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let allData: Item[] = [];
+
+      // Initial request
+      let response = await axios.get('https://swapi.dev/api/vehicles/');
+      let apiData: { results: Item[]; next: string } = response.data;
+      allData = allData.concat(apiData.results);
+
+      // Loop through additional pages
+      while (apiData.next) {
+        response = await axios.get(apiData.next);
+        apiData = response.data;
+        allData = allData.concat(apiData.results);
+      }
+
+      setData(allData);
+    };
+
+    fetchData().catch(err => console.log(err));
+  }, []);
+
+  const handleSelectChange = (event: SelectChangeEvent<string>) => {
+    setSelectedItem(event.target.value as string);
+  };
+  
+
+  return (
+    <div className="background-image">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+      
+      <div className="center-container">
+    {/* Render the select */}
+    <FormControl variant="outlined">
+      <InputLabel id="select-label">Select a vehicle</InputLabel>
+      <Select
+        labelId="select-label"
+        id="select-vehicle"
+        value={selectedItem}
+        onChange={handleSelectChange}
+        label="Select a vehicle"
+        sx={{ minWidth: '400px', marginBottom: '15px' }}
+      >
+        {data.map((item) => (
+          <MenuItem key={item.id} value={item.name}>
+            {item.name}
+          </MenuItem>
+        ))}
+      </Select>
+      <Box textAlign='center' sx={{ '& button': { m: 1 } }}>
+      <div>
+      <Link to="/checkout">
+      <Button 
+        variant="contained" 
+        size="small" 
+        color="primary"
+        sx={{
+        backgroundColor: '#000000', '&:hover': {backgroundColor: '#666'},
+      }}>
+          Checkout
+        </Button>
+        </Link>
+      </div>
+    </Box>
+    </FormControl>
+    </div>
+    </header>
+    </div>
+  );
+};
+
+export default VehiclesList;
